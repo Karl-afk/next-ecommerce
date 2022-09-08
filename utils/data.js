e1c9887fe7b3ef72968e155d5c0bd439d51ea8e2
Karl Weinmueller <karlweinmueller@me.com>
2022-09-08 21:25:16 +0200

    added db connection

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Joe',
+      email: 'joe@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'jane@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Free Shirt',
